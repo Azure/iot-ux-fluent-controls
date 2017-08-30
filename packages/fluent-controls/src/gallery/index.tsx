7e03094139f79d76aa8aac5024bbff776d749411
Alexander Kiselev (Akvelon INC) <v-alkise@microsoft.com>
2017-08-30 09:02:15 -0700

    removing forms for pull request approval

--- packages/fluent-controls/src/gallery/index.tsx
+++ packages/fluent-controls/src/gallery/index.tsx
@@ -1,25 +1,25 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import {FormTest} from './pages/Form';
+import {CardTest} from './pages/GalleryCard';
  
 const HelloWorld = _ => <h1>Hello, world!</h1>;
 
-ReactDOM.render(<FormTest />, document.getElementById('content'));
+ReactDOM.render(<CardTest />, document.getElementById('content'));
 
 const rootEl = document.getElementById('content');
 
 ReactDOM.render(
   <AppContainer>
-    <FormTest />
+    <CardTest />
   </AppContainer>,
   rootEl
 );
 
 // Hot Module Replacement API
 if (module.hot) {
-  module.hot.accept('./pages/Form', () => {
-    const NextApp = require<{FormTest: typeof FormTest}>('./pages/Form').FormTest;
+  module.hot.accept('./pages/GalleryCard', () => {
+    const NextApp = require<{FormTest: typeof CardTest}>('./pages/GalleryCard').FormTest;
     ReactDOM.render(
       <AppContainer>
         <NextApp />
