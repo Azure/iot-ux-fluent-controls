f4d2c8c892659a51c9855c61f953492781e51160
Alexander Kiselev (Akvelon INC) <v-alkise@microsoft.com>
2017-09-18 17:46:57 -0700

    working on ComboInput using map/filter/select functions

--- packages/fluent-controls/src/components/Form/ComboInput.md
+++ packages/fluent-controls/src/components/Form/ComboInput.md
@@ -1,6 +1,15 @@
 ```jsx
-let initialState = {value: ''};
+let initialState = {
+    value: '',
+    options: [
+        {label: 'Label 1', value: {str: 'Option 1'}},
+        {label: 'Label 2', value: {str: 'Option 2'}},
+        {label: 'Label 3', value: {str: 'Option 3'}},
+        {label: 'Label 4', value: {str: 'Option 4'}},
+        {label: 'Label 5', value: {str: 'Option 5'}},
+    ]
+};
 let onChange = (newValue) => setState({value: newValue});
 
-<TextInput name="textinput" value={state.value} onChange={onChange}/>
+<ComboInput name="combo-input" options={state.options} value={state.value} onChange={onChange} optionsMap={opt => opt.str}/>
 ```
--- packages/fluent-controls/src/components/Form/ComboInput.tsx
+++ packages/fluent-controls/src/components/Form/ComboInput.tsx
@@ -4,12 +4,6 @@ import {Icon, IconSize} from '../Icon';
 import {MethodNode, FormOption} from '../../Common';
 const css = classNames.bind(require('./ComboInput.scss'));
 
-
-export interface ComboResolver {
-    onChange: (newValue: string, options: FormOption[]) => FormOption[] | Promise<FormOption[]>;
-    getList: () => FormOption[];
-}
-
 export interface ComboInputType {}
 
 export interface ComboInputProps extends React.Props<ComboInputType> {
@@ -21,7 +15,7 @@ export interface ComboInputProps extends React.Props<ComboInputType> {
     placeholder?: string;
 
     /** 
-     * List of HTML radio button element options in the format:
+     * List of HTML select element options in the format:
      * 
      * `{
      *     label: string,
@@ -31,6 +25,9 @@ export interface ComboInputProps extends React.Props<ComboInputType> {
      * }`
      */
     options: FormOption[];
+    optionsMap?: (option: FormOption) => string;
+    optionsFilter?: (newValue: string, option: string) => boolean;
+    optionsSelect?: (newValue: string, option: string) => boolean;
 
     /** Apply error styling to input element */
     error?: boolean;
@@ -38,17 +35,27 @@ export interface ComboInputProps extends React.Props<ComboInputType> {
     disabled?: boolean;
 
     /** Callback for HTML input element `onChange` events */
-    onChange: (newValue: string) => void;
-    /** Interface for autocomplete functions */
-    resolver: ComboResolver;
+    onChange: (newValue: string | FormOption) => void;
 
     /** Class to append to top level element */
     className?: string;
 }
 
 export interface ComboInputState {
-
+    visible: boolean;
 }
+
+const defaultMap = (option: FormOption) => {
+    if (typeof(option.value) === 'string') {
+        return option.value;
+    }
+    console.error('The default ComboInput map function expects FormOption.value to be a string');
+    return null;
+};
+
+const defaultFilter = (option: string, newValue: string) => true;
+
+const defaultSelect = (option: string, newValue: string) => option === newValue;
 
 /**
  * Low level combo input control
@@ -56,10 +63,30 @@ export interface ComboInputState {
  * (Use the `ComboField` control instead when making a form with standard styling)
  */
 export class ComboInput extends React.Component<ComboInputProps, ComboInputState> {
+    static defaultProps =  {
+        optionsFilter: defaultFilter,
+        optionsMap: defaultMap,
+        optionsSelect: defaultSelect
+    };
+
     constructor(props: ComboInputProps) {
         super(props);
 
-        this.state = {};
+        this.state = {
+            visible: false
+        };
+    }
+
+    onFocus() {
+        this.setState({visible: true});
+    }
+
+    onBlur() {
+        this.setState({visible: false});
+    }
+
+    onSelect(value: string | FormOption) {
+
     }
 
     render () {
@@ -70,11 +97,37 @@ export class ComboInput extends React.Component<ComboInputProps, ComboInputState
         });
         
         const onChange = (event) => {
-            if (this.props.value !== event.target.value) {
-                this.props.onChange(event.target.value);
+            const newValue = event.target.value;
+            const options = this.props.options.map(this.props.optionsMap);
+            const result = options.filter(option => this.props.optionsSelect(option, newValue));
+            if (result.length === 0) {
+                this.props.onChange(result[0]);
+            } else {
+                this.props.onChange(newValue);
             }
-            event.stopPropagation();
         };
+
+        const options = [];
+        for (let index = 0; index < this.props.options.length; index++) {
+            const option = this.props.options[index];
+            const strValue = this.props.optionsMap(option);
+            const visible = this.props.optionsFilter(strValue, this.props.value);
+            const onClick = (event) => {
+                this.onSelect(option);
+            };
+            const optionClassName = css('combo-option', {
+                'selected': this.props.optionsSelect(this.props.value, strValue)
+            });
+        
+
+            if (visible) {
+                options.push(
+                    <div className={optionClassName} onClick={onClick} key={index}>
+                        {option.label}
+                    </div>
+                );
+            }
+        }
 
         const clearButton = this.props.disabled ? '' :
             <button
@@ -91,19 +144,24 @@ export class ComboInput extends React.Component<ComboInputProps, ComboInputState
                         name={this.props.name}
                         value={this.props.value}
                         className={inputClassName}
-                        onInput={onChange}
+                        onChange={onChange}
                         placeholder={this.props.placeholder}
-                        // This is not the same as props.required
+                        onFocus={event => this.onFocus()}
+                        onBlur={event => this.onBlur()}
+                        // This is not the same as this.props.required
                         // (this gives us :valid css selector)
                         required
                         disabled={this.props.disabled}
                     />
                     <Icon
-                        icon='chevronDown'
+                        icon='chevronDown4'
                         size={IconSize.xsmall}
                         className={css('chevron')}
                     />
                     {clearButton}
+                    <div className={css('dropdown', {'visible': this.state.visible})}>
+                        {options}
+                    </div>
                 </div>
             </div>
         );
