d98e9def311d850b3a577e3a355e0b4ff17ce13c
Alexander Kiselev (Akvelon INC) <v-alkise@microsoft.com>
2017-09-26 10:23:11 -0700

    added auto grow

--- packages/fluent-controls/src/components/Form/TextArea.tsx
+++ packages/fluent-controls/src/components/Form/TextArea.tsx
@@ -10,11 +10,15 @@ export interface TextAreaProps extends React.Props<TextAreaType> {
     name: string;
     /** Current value of HTML input element */
     value?: string;
+    /** Text area placeholder */
+    placeholder?: string;
     
     /** Apply error styling to input element */
     error?: boolean;
     /** Disable HTML input element and apply disabled styling */
     disabled?: boolean;
+    /** Grow text area to fit user text */
+    autogrow?: boolean;
 
     /** Callback for HTML input element `onChange` events */
     onChange: (newValue: string) => void;
@@ -23,27 +27,59 @@ export interface TextAreaProps extends React.Props<TextAreaType> {
     className?: string;
 }
 
+export interface TextAreaState {
+}
+
 /**
  * Low level text input control
  * 
  * (Use the `TextField` control instead when making a form with standard styling)
  */
-export const TextArea: React.StatelessComponent<TextAreaProps> = (props: TextAreaProps) => {
-    return (
-        <textarea
-            name={props.name}
-            value={props.value}
-            className={css('textarea', {'error': props.error})}
-            onChange={event => props.onChange(event.target.value)}
-            disabled={props.disabled}
-        />
-    );
-};
+export class TextArea extends React.Component<TextAreaProps, TextAreaState> {
+    static defaultProps = {
+        autogrow: true,
+        error: false,
+        disabled: false,
+        value: ''
+    };
 
-TextArea.defaultProps = {
-    error: false,
-    disabled: false,
-    value: ''
-};
+    private textarea: HTMLTextAreaElement;
+    private ghost: HTMLPreElement;
+
+    constructor(props: TextAreaProps) {
+        super(props);
+    }
+
+    componentDidUpdate(prevProps: TextAreaProps, prevState: TextAreaState) {
+        const height = this.ghost.offsetHeight;
+        if (prevProps.value !== this.props.value && height > 52) {
+            this.textarea.style.height = `${height}px`;
+        }
+    }
+
+    render() {
+        const value = this.props.value;
+
+        return (
+            <div className={css('textarea-container')}>
+                <textarea
+                    name={this.props.name}
+                    value={value}
+                    className={css('textarea', {'error': this.props.error})}
+                    onChange={event => this.props.onChange(event.target.value)}
+                    disabled={this.props.disabled}
+                    placeholder={this.props.placeholder}
+                    ref={element => this.textarea = element}
+                />
+                <pre
+                    className={css('textarea', 'textarea-ghost')}
+                    ref={element => this.ghost = element}
+                >
+                    {value + (value[value.length - 1] === '\n' ? '\n' : '')}
+                </pre>
+            </div>
+        );
+    }
+}
 
 export default TextArea;
