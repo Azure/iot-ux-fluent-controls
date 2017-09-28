08ace7f289428782f3fab04e02b595a29f18ead3
Alexander Kiselev (Akvelon INC) <v-alkise@microsoft.com>
2017-09-28 15:35:56 -0700

    wrote first typing tests for DatePicker

--- packages/fluent-controls/src/components/DateTime/DatePicker.spec.tsx
+++ packages/fluent-controls/src/components/DateTime/DatePicker.spec.tsx
@@ -1,35 +1,69 @@
 import * as React from 'react';
-import * as chaiEnzyme from 'chai-enzyme';
-import { expect } from 'chai';
-import { mount } from 'enzyme';
-import {  } from 'karma';
+import * as sinon from 'sinon';
+import { expect, assert } from 'chai';
+import { mount, ReactWrapper } from 'enzyme';
+import { DatePicker, DatePickerProps, DateFormat } from './DatePicker';
 
-import {DatePicker, DateFormat} from './DatePicker';
+class DatePickerWrapper extends ReactWrapper<DatePickerProps, any> {
+    input: any;
 
-const setup = (initialValue, newProps) => {
-  const props = {
-    name: 'date-picker',
-    initialValue,
-    tabIndex: -1,
+    constructor(props: DatePickerProps) {
+        super(<DatePicker {...props}/>);
+        this.input = this.find('input');
+    }
 
-    ...newProps
-  };
+    focus() {
+        this.input.simulate('focus');
+    }
 
-  const wrapper = mount(<DatePicker {...props} />);
+    change(newValue: string) {
+        this.input.simulate('input', {target: {value: newValue}});
+    }
 
-  return {
-    props,
-    wrapper
-  };
+    paste(newValue: string) {
+        this.input.simulate('paste');
+    }
+
+    get value(): any {
+        return this.state('value');
+    }
+}
+
+const testDatePickerInput = (wrapper: any, onChange: any, values: string[][]) => {
+    for (let index in values) {
+        wrapper.change(values[index][0]);
+        if (values[index][1]) {
+            expect(wrapper.value).to.equal(values[index][1]);
+        } else {
+            expect(wrapper.value).to.equal(values[index][0]);
+        }
+        expect(onChange.args[index][0]).to.equal(values[index][2]);
+    }
 };
 
 describe('DatePicker', () => {
-  it('should handle typing', () => {
-    const { props, wrapper } = setup('Thu, 28 Sep 2017 17:28:40 GMT', {
-        localTimezone: true,
-        format: DateFormat.DDMMYYYY
-    });
+    it('should handle typing with initial value', () => {
+        const onChange = sinon.spy();
+        const wrapper = new DatePickerWrapper({
+            name: 'date-picker',
+            initialValue: 'Thu, 28 Sep 2017 17:28:40 GMT',
+            localTimezone: true,
+            format: DateFormat.DDMMYYYY,
+            onChange: onChange
+        });
 
-    expect(wrapper.find('input').value).toEqual('28/09/2017');
-  });
+        expect(wrapper.value).to.equal('28/09/2017');
+
+        
+
+        wrapper.focus();
+        expect(wrapper.state('visible')).to.equal(true);
+
+        testDatePickerInput(wrapper, onChange, [
+            ['28/09/201', '', 'invalid'],
+            ['28/09/2018', '', 'Fri, 28 Sep 2018 17:28:40 GMT'],
+            ['2/09/2018', '', 'invalid'],
+            ['21/09/2018', '', 'Fri, 21 Sep 2018 17:28:40 GMT'],            
+        ]);
+    });
 });
--- packages/fluent-controls/tests.bundle.js
+++ packages/fluent-controls/tests.bundle.js
@@ -2,7 +2,7 @@ var context = require.context('./src/components/', true, /.+\.tsx?$/);
 context.keys().forEach(context);
 module.exports = context;
 
-const Enzyme = require('enzyme').Enzyme;
-const Adapter = require('enzyme-adapter-react-15').Adapter;
+const Enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-15');
 
 Enzyme.configure({adapter: new Adapter()});
