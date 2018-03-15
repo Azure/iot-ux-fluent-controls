f3c159aa696638d9002104e1b3ff71d045234176
fernandoviton <fcode@viton.org>
2018-03-15 13:57:09 -0700

    Move examples to dir and create example structure for them (#7)

--- src/index.tsx
+++ src/index.tsx
@@ -2,29 +2,9 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { FormOption } from './Common';
 import ComboInput from './components/Input/ComboInput';
-//import './index.css';
+import Examples from './examples';
 
-const comboInputOptions = [
-  {label: 'Label 1', value: 'Option 1'} as FormOption,
-  {label: 'Label 2', value: 'Option 2'} as FormOption
-];
-
-const makeComboOptions = labels => labels.map(label => ({
-  label: label,
-  value: label
-}) as FormOption)
-
-const App = () => (
-  <div>
-    <ComboInput
-      name='combo-input'
-      value='value'
-      onChange={()=>{}}
-      options={comboInputOptions}/>
-  </div>);
-
-// TODO: move all this into an example dir with seperate build
 ReactDOM.render(
-  <App />,
+  <Examples />,
   document.getElementById('root') as HTMLElement
 );
