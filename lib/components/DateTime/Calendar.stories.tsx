11a1a922668687fdc655ffd100ecdc69cd91e19f
Michael Steinberg <misteinb@microsoft.com>
2018-10-01 17:09:44 -0700

    add state to calendar

--- lib/components/DateTime/Calendar.stories.tsx
+++ lib/components/DateTime/Calendar.stories.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Calendar } from './Calendar';
-import { action } from '@storybook/addon-actions'
+import { Store, State } from '@sambego/storybook-state';
+
+const store = new Store({
+    value: new Date().toISOString()
+});
 
 storiesOf('Calendar', module)
-    .add('default (no initial value)', () => (
-        <div style={{width: '260px', border: '1px solid #333', margin: '0 20px'}}>
-            <Calendar onChange={action('date changed')}/>
-        </div>
-    )).add('with initial value', () => (
-        <div style={{width: '260px', border: '1px solid #333', margin: '0 20px'}}>
-            <Calendar value={new Date().toISOString()} onChange={action('date changed')}/>
-        </div>
-    ));
+    .add('default', () => {
+        return (
+            <div style={{ width: '260px', border: '1px solid #333', margin: '0 20px' }}>
+                <State store={store}>
+                    <Calendar value={store.get('value')} onChange={value => store.set({ value })} />
+                </State>
+            </div>
+        );
+    });
