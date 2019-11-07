7b10190d48191ea5fe1d1603e0193f47ea912862
Patricio Beltran <pbelsal@gmail.com>
2019-11-07 12:17:44 -0800

    Style fixes and adding max height for edge case scenario of balloon

--- lib/components/Dropdown/Dropdown.tsx
+++ lib/components/Dropdown/Dropdown.tsx
@@ -149,16 +149,32 @@ export const Dropdown = React.memo(({ className, onMouseEnter, onMouseLeave, att
                     containerRef.current.style.left = `${dropdownLeftForStart}px`;
                 } else if (dropdownAlignment.current === DropdownAlignment.Center && dropdownLeftForCenter > 0 && dropdownLeftForCenter + dropdownRect.width <= window.innerWidth) {
                    containerRef.current.style.left = `${dropdownLeftForCenter}px`; 
-                } else if (dropdownAlignment.current === DropdownAlignment.End && hostRect.left - dropdownLeftForEnd > 0) {
+                } else if (dropdownAlignment.current === DropdownAlignment.End && dropdownLeftForEnd > 0) {
                     containerRef.current.style.left = `${dropdownLeftForEnd}px`;
                 } else {
                     if (dropdownLeftForStart + dropdownRect.width <= window.innerWidth) {
                         containerRef.current.style.left = `${dropdownLeftForStart}px`;
                         dropdownAlignment.current = DropdownAlignment.Start;
+                        dropdownRef.current.style.maxWidth = undefined;
                     }
-                    else {
+                    else if (dropdownLeftForEnd > 0) {
                         containerRef.current.style.left = `${dropdownLeftForEnd}px`;
                         dropdownAlignment.current = DropdownAlignment.End;
+                        dropdownRef.current.style.maxWidth = undefined;
+                    } else {
+                        // it doesn't fit in any of the two places, pick the one with the most space and set a max width to the dropdown
+                        const maxWidthForEnd = dropdownRect.width + dropdownLeftForEnd;
+                        const maxWidthForStart = window.innerWidth - dropdownLeftForStart;
+
+                        if (maxWidthForEnd > maxWidthForStart) {
+                            dropdownAlignment.current = DropdownAlignment.End;
+                            containerRef.current.style.left = `${dropdownLeftForEnd}px`;
+                            dropdownRef.current.style.maxWidth = `${maxWidthForEnd}px`;
+                        } else {
+                            dropdownAlignment.current = DropdownAlignment.Start;
+                            containerRef.current.style.left = `${dropdownLeftForStart}px`;
+                            dropdownRef.current.style.maxWidth = `${maxWidthForStart}px`;
+                        }
                     }
                 }
 
@@ -172,14 +188,31 @@ export const Dropdown = React.memo(({ className, onMouseEnter, onMouseLeave, att
                     containerRef.current.style.top = `${dropdownTopForTop}px`;
                 } else {
                     // We need to update the position
-                    if (dropdownTopForBottom + dropdownRect.height <= window.innerHeight) {
-                        containerRef.current.style.top = `${dropdownTopForBottom}px`;
-                        dropdownPosition.current = DropdownPosition.Bottom;
-                    } else {
+                    if (dropdownTopForTop > 0 ) {
                         containerRef.current.style.top = `${dropdownTopForTop}px`;
                         dropdownPosition.current = DropdownPosition.Top;
+                        dropdownRef.current.style.maxHeight = undefined;
+                    } else if (dropdownTopForBottom + dropdownRect.height <= window.innerHeight) {
+                        containerRef.current.style.top = `${dropdownTopForBottom}px`;
+                        dropdownPosition.current = DropdownPosition.Bottom;
+                        dropdownRef.current.style.maxHeight = undefined;
+                    } else {
+                        // it doesn't fit in any of the two places, pick the one with the most space and set a max height to the dropdown
+                        const maxHeightForTop = dropdownRect.height + dropdownTopForTop;
+                        const maxHeightForBottom = window.innerHeight - dropdownTopForBottom;
+
+                        if (maxHeightForBottom > maxHeightForTop) {
+                            dropdownPosition.current =  DropdownPosition.Bottom;
+                            containerRef.current.style.top = `${dropdownTopForBottom}px`;
+                            dropdownRef.current.style.maxHeight = `${maxHeightForBottom}px`;
+                        } else {
+                            dropdownPosition.current =  DropdownPosition.Top;
+                            containerRef.current.style.top = `${dropdownTopForTop}px`;
+                            dropdownRef.current.style.maxHeight = `${maxHeightForTop}px`;
+                        }
                     }
                 }
+            // Repeat same logic for right/left
             } else {
                 const alignmentArrowOffset = showArrow ? ArrowSize / 2 + ArrowMargin : 0;
                 const dropdownTopForStart = hostRect.top + hostRect.height / 2 - alignmentArrowOffset - marginOffset;
@@ -193,13 +226,28 @@ export const Dropdown = React.memo(({ className, onMouseEnter, onMouseLeave, att
                 } else if (dropdownAlignment.current === DropdownAlignment.End && dropdownTopForEnd > 0) {
                     containerRef.current.style.top = `${dropdownTopForEnd}px`;
                 } else {
-                    if (dropdownTopForStart + dropdownRect.height <= window.innerHeight) {
-                        containerRef.current.style.top = `${dropdownTopForStart}px`;
-                        dropdownAlignment.current = DropdownAlignment.Start;
-                    }
-                    else {
+                    if (dropdownTopForEnd > 0) {
                         containerRef.current.style.top = `${dropdownTopForEnd}px`;
                         dropdownAlignment.current = DropdownAlignment.End;
+                        dropdownRef.current.style.maxHeight = undefined;
+                    } else if (dropdownTopForStart + dropdownRect.height <= window.innerHeight) {
+                        containerRef.current.style.top = `${dropdownTopForStart}px`;
+                        dropdownAlignment.current = DropdownAlignment.Start;
+                        dropdownRef.current.style.maxHeight = undefined;
+                    }
+                    else {
+                        const maxHeightForEnd = dropdownRect.height + dropdownTopForEnd;
+                        const maxHeightForStart = window.innerHeight - dropdownTopForStart;
+
+                        if (maxHeightForEnd > maxHeightForStart) {
+                            containerRef.current.style.top = `${dropdownTopForEnd}px`;
+                            dropdownAlignment.current = DropdownAlignment.End;
+                            dropdownRef.current.style.maxHeight = `${maxHeightForEnd}px`;
+                        } else {
+                            containerRef.current.style.top = `${dropdownTopForStart}px`;
+                            dropdownAlignment.current = DropdownAlignment.Start;
+                            dropdownRef.current.style.maxHeight = `${maxHeightForStart}px`;
+                        }
                     }
                 }
 
@@ -216,9 +264,24 @@ export const Dropdown = React.memo(({ className, onMouseEnter, onMouseLeave, att
                     if (dropdownLeftForLeft > 0) {
                         containerRef.current.style.left = `${dropdownLeftForLeft}px`;
                         dropdownPosition.current = DropdownPosition.Left;
-                    } else {
+                        dropdownRef.current.style.maxWidth = undefined;
+                    } else if (dropdownLeftForRight + dropdownRect.width <= window.innerWidth) {
                         containerRef.current.style.left = `${dropdownLeftForRight}px`;
                         dropdownPosition.current = DropdownPosition.Right;
+                        dropdownRef.current.style.maxWidth = undefined;
+                    } else {
+                        const maxWidthForLeft = dropdownRect.width + dropdownLeftForLeft;
+                        const maxWidthForRight = window.innerWidth - dropdownLeftForRight;
+
+                        if (maxWidthForLeft > maxWidthForRight) {
+                            containerRef.current.style.left = `${dropdownLeftForLeft}px`;
+                            dropdownPosition.current = DropdownPosition.Left;
+                            dropdownRef.current.style.maxWidth = `${maxWidthForLeft}px`;
+                        } else {
+                            containerRef.current.style.left = `${dropdownLeftForRight}px`;
+                            dropdownPosition.current = DropdownPosition.Right;
+                            dropdownRef.current.style.maxWidth = `${maxWidthForRight}px`;
+                        }
                     }
                 }
             }
