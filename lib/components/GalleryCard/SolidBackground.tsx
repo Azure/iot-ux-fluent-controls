c2385e51479a687c13bb8330e7295f6db275ee7e
Patricio Beltran <pbelsal@gmail.com>
2020-03-30 12:41:34 -0700

    Update gallery card solid background to not accept hardcoded colors (#188)

--- lib/components/GalleryCard/SolidBackground.tsx
+++ lib/components/GalleryCard/SolidBackground.tsx
@@ -3,26 +3,27 @@ import * as classNames from 'classnames/bind';
 import {DivProps, Elements as Attr} from '../../Attributes';
 const css = classNames.bind(require('./GalleryCard.module.scss'));
 
-const defaultColor = '#eaeaea';
-
 export interface SolidBackgroundAttributes {
     container?: DivProps;
 }
 
 export interface SolidBackgroundProps {
     /**
-     * Background color (accepts string color names and RGB hex values)
-     *
-     * Default: #eaeaea
-     */
-    backgroundColor?: string;
-
-    /**
      * Fixed width and height (284 x ?? pixels)
      *
      * Default: true
      */
     fixed?: boolean;
+
+    /**
+     * Indicates if the background should set primary styles (use accent colors)
+     */
+    primary?: boolean;
+
+    /**
+     * Indicates if the background should set disabled styles
+     */
+    disabled?: boolean;
 
     /** Classname to append to top level element */
     className?: string;
@@ -39,26 +40,16 @@ export interface SolidBackgroundProps {
  *
  * @param props Control properties (Defined in `ImageBackgroundProps` interface)
  */
-export const SolidBackground = React.memo((props: SolidBackgroundProps) => {
-    const bgColor = props.backgroundColor ?? defaultColor;
-    const isClass = bgColor ? bgColor.substr(0, 1) !== '#' : false;
-
-    const cls = css(
-        {
-            'background-color': true,
-            'fixed': props.fixed == null || props.fixed,
-            [bgColor]: isClass
-        },
-        props.className
-    );
-
-    const style = isClass 
-        ? { backgroundColor: bgColor }
-        : undefined;
+export const SolidBackground = React.memo(({ fixed, primary, disabled, children, className, attr }: SolidBackgroundProps) => {
+    const cls = css('background-color', { 
+        fixed: fixed == null || fixed, 
+        primary,
+        disabled
+    }, className);
 
     return (
-        <Attr.div className={cls} style={style} attr={props.attr?.container}>
-            {props.children}
+        <Attr.div className={cls} attr={attr?.container}>
+            {children}
         </Attr.div>
     );
 });
